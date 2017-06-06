898b5adcf8a41b73056e728cfdada3b7be9dab11
Julien Reboul <julien.reboul@bonitasoft.com>
2017-06-06 22:44:18 +0200

    listen only event when mousedown on element has happened

--- src/app/resize-handle.directive.ts
+++ src/app/resize-handle.directive.ts
@@ -1,4 +1,7 @@
 import { Directive, ElementRef, HostListener, OnInit, EventEmitter } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/observable/fromEvent';
 
 @Directive({
   selector: '[resize-handle]'
@@ -8,6 +11,8 @@ export class ResizeHandleDirective {
   private mouseDown: boolean = false;
   private width: number;
   private height: number;
+  private mouseUpSubscription: Subscription;
+  private mouseMoveSubscription: Subscription;
   public mouseMove$: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
   public mouseDown$: EventEmitter<string> = new EventEmitter<string>();
   constructor(private element: ElementRef) { }
@@ -17,19 +22,29 @@ export class ResizeHandleDirective {
   }
 
   @HostListener('mousedown') onMouseDown() {
-    this.mouseDown = true;
+    this.unsubscribeFromEvent();
+    this.mouseMoveSubscription = Observable.fromEvent(document, 'mousemove').subscribe((event: MouseEvent) => this.onMouseMove(event));
+    this.mouseUpSubscription = Observable.fromEvent(document, 'mouseup').subscribe(() => this.onMouseUp());
   }
-  @HostListener('document:mouseup') onMouseUp() {
-    console.log('mouseup!');
-    this.mouseDown = false;
+  onMouseUp() {
+    this.unsubscribeFromEvent();
   }
-  @HostListener('document:mousemove', ['$event']) onMouseMove(event: MouseEvent) {
-    if (this.mouseDown) {
-      console.log('mousemove!');
-      this.mouseMove$.next(event);
-      event.preventDefault();
-      event.stopPropagation();
+
+  unsubscribeFromEvent() {
+    if (this.mouseMoveSubscription) {
+      this.mouseMoveSubscription.unsubscribe();
+      this.mouseMoveSubscription = null;
     }
+    if (this.mouseUpSubscription) {
+      this.mouseUpSubscription.unsubscribe();
+      this.mouseUpSubscription = null;
+    }
+  }
+  onMouseMove(event: MouseEvent) {
+    console.log('mousemove!');
+    this.mouseMove$.next(event);
+    event.preventDefault();
+    event.stopPropagation();
   }
   topRightResize(offsetX: number, offsetY: number) {
   }
